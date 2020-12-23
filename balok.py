b2b34aa9b29de2c790ec244ef8ecdb355a94ce1c
ibnudafa31 <ibnudafa31@gmail.com>
2020-12-23 16:24:00 +0700

    menambahkan file balok

---
+++ balok.py
@@ -0,0 +1,13 @@
+# Ibnu Dafa
+
+def _volume_balok(P,L,T):
+    i=4*(P+L+T)
+    return i
+
+P = float(input("masukkan nilai P:"))
+L = float(input("masukkan nilai L:"))
+T = float(input("masukkan nilai T:"))
+
+print("_volume_balok", _volume_balok(P,L,T)) 
+
+
