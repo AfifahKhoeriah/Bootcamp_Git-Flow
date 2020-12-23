1df58f99d8fc9b807409d391fcfa16619c8104a2
AfifahKhoeriah <afififahkhoeriah@gmail.com>
2020-12-23 20:56:21 +0700

    complete main program

--- main_luas.py
+++ main_luas.py
@@ -1,10 +1,22 @@
 # main
 def main():
-    pilih = input('Pilih Luas Bangun Ruang Apa? [Kubus, Prisma Segitiga] ')
-    if pilih == 'Kubus':
+    pilih = input('Pilih Luas Bangun Ruang Apa? \n1. Kubus\n2. Balok\n3. Kerucut\n4. Bola \n5. Tabung\n6. Limas Segitiga\n7. Prisma Segitiga\n')
+    if pilih in ["Kubus","kubus","1"]:
         print("\nLuas Kubus Adalah:  ", kubus().luas_kubus())
+    elif pilih in ["Balok","balok","2"]:
+        print("\nLuas Balok Adalah:  ", balok().luas_balok())
+    elif pilih in ["Kerucut","kerucut","3"]:
+        print("\nLuas Kerucut Adalah:  ", LuasKerucut().luas())    
+    elif pilih in ["Bola","bola","4"]:
+        print("\nLuas Bola Adalah:  ", LuasBola().luas())  
+    elif pilih in ["Tabung","tabung","5"]:
+        print("\nLuas Tabung Adalah:  ", Tabung().luasTabung())  
+    elif pilih in ["Limas Segitiga","limas segitiga","6"]:
+        print("\nLuas Limas Segitiga Adalah:  ", LimasSegitiga().luasLimasSegitiga()) 
+    elif pilih in ["Prisma Segitiga","prisma segitiga","7"]:
+        print("\nLuas Prisma Segitiga Adalah:  ", prisma_segitiga().luas_prisma_segitiga()) 
     else:
-        print("\nLuas Prisma Segitiga Adalah:  ", prisma_segitiga().luas_prisma_segitiga())
+        print("Masukkan inputan yang benar ")
 
 # run program
 if __name__ == "__main__":
--- main_volume.py
+++ main_volume.py
@@ -1,10 +1,22 @@
 # main
 def main():
-    pilih = input('Pilih Volume Bangun Ruang Apa? [Kubus, Prisma Segitiga] ')
-    if pilih == 'Kubus':
+    pilih = input('Pilih Volume Bangun Ruang Apa? \n1. Kubus\n2. Balok\n3. Kerucut\n4. Bola \n5. Tabung\n6. Limas Segitiga\n7. Prisma Segitiga\n')
+    if pilih in ["Kubus","kubus","1"]:
         print("\nVolume Kubus Adalah:  ", kubus().volume_kubus())
+    elif pilih in ["Balok","balok","2"]:
+        print("\nVolume Balok Adalah:  ", balok().volume_balok())
+    elif pilih in ["Kerucut","kerucut","3"]:
+        print("\nVolume Kerucut Adalah:  ", VolKerucut().volume())    
+    elif pilih in ["Bola","bola","4"]:
+        print("\nVolume Bola Adalah:  ", VolBola().volume())  
+    elif pilih in ["Tabung","tabung","5"]:
+        print("\nVolume Tabung Adalah:  ", Tabung().volumeTabung())  
+    elif pilih in ["Limas Segitiga","limas segitiga","6"]:
+        print("\nVolume Limas Segitiga Adalah:  ", LimasSegitiga().volumeLimasSegitiga()) 
+    elif pilih in ["Prisma Segitiga","prisma segitiga","7"]:
+        print("\nVolume Prisma Segitiga Adalah:  ", prisma_segitiga().Volume_prisma_segitiga()) 
     else:
-        print("\nVolume Prisma Segitiga Adalah:  ", prisma_segitiga().Volume_prisma_segitiga())
+        print("Masukkan inputan yang benar ")
 
 # run program
 if __name__ == "__main__":
