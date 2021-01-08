820c44f16374500cf6aa6366e8a8416a55d74ad9
anisaiue <anisans1411@gmail.com>
2021-01-08 14:20:52 +0700

    First modified commit, Anisa

--- luas_kerucut_bola.py
+++ luas_kerucut_bola.py
@@ -1,16 +1,18 @@
+#Membuat class untuk Luas Kerucut
 class LuasKerucut:
     def __init__(self):
         self.radius = float(input("Masukkan radius kerucut: "))
         self.pelukis = float(input("Masukkan garis pelukis kerucut: "))
 
     def luas(self):
-        phi = 3.14
-        return (phi*(self.radius))*(self.radius +self.pelukis)
+        pi = 3.14
+        return (pi*(self.radius))*(self.radius +self.pelukis)
 
+#Membuat class untuk Luas Bola
 class LuasBola:
     def __init__(self):
         self.radius = float(input("Masukkan radius bola: "))
 
     def luas(self):
-        phi = 3.14
-        return 4*phi*(self.radius**2)
+        pi = 3.14
+        return 4*pi*(self.radius**2)
